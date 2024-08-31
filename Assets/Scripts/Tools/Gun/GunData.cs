a5c66602da2cbd36f778150786522d4a9419e2b9
TayfurSafakGencay <safak.gencay@gmail.com>
2024-08-31 09:38:25 +0300

    New Tools and Data added. Unnecessary scripts removed.

---
+++ Assets/Scripts/Tools/Gun/GunData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Managers;
+using UnityEngine;
+
+namespace Tools.Gun
+{
+  [CreateAssetMenu(fileName = "GunData", menuName = "Tools/Gun/Create Gun Data", order = 0)]
+  public class GunData : ScriptableObject
+  {
+    public List<GunVo> GunVos;
+  }
+}
---
+++ Assets/Scripts/Tools/Panel/PanelData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Managers;
+using UnityEngine;
+
+namespace Tools.Panel
+{
+  [CreateAssetMenu(fileName = "Panel Data", menuName = "Tools/Panel/Create Panel Data", order = 0)]
+  public class PanelData : ScriptableObject
+  {
+    [SerializeField]
+    private List<PanelVO> PanelsData;
+
+    public readonly Dictionary<PanelKey, List<GameState>> Panels = new();
+
+    public void Init()
+    {
+      for (int i = 0; i < PanelsData.Count; i++)
+      {
+        PanelVO panelVo = PanelsData[i];
+        if (!Panels.ContainsKey(panelVo.PanelKey))
+        {
+          Panels.Add(panelVo.PanelKey, panelVo.GameStates);
+        }
+      }
+    }
+  }
+
+  [Serializable]
+  public struct PanelVO
+  {
+    public PanelKey PanelKey;
+
+    public List<GameState> GameStates;
+  }
+}
